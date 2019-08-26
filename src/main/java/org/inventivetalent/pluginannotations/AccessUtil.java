6644890e897e0bd1a95203bd1ac13fd944d65a8d
inventivetalent <mail@inventivetalent.org>
2019-08-26 21:56:11 +0200

    wrap final removal in an extra try/catch block since JDK 12 doesn't seem to support that

--- src/main/java/org/inventivetalent/pluginannotations/AccessUtil.java
+++ src/main/java/org/inventivetalent/pluginannotations/AccessUtil.java
@@ -10,12 +10,20 @@ public class AccessUtil {
 	 *
 	 * @param f Field set accessible
 	 */
-	public static Field setAccessible(Field f) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-		f.setAccessible(true);
-		Field modifiersField = Field.class.getDeclaredField("modifiers");
-		modifiersField.setAccessible(true);
-		modifiersField.setInt(f, f.getModifiers() & 0xFFFFFFEF);
-		return f;
+	public static Field setAccessible(Field field) throws ReflectiveOperationException {
+		field.setAccessible(true);
+		try {
+			Field modifiersField = Field.class.getDeclaredField("modifiers");
+			modifiersField.setAccessible(true);
+			modifiersField.setInt(field, field.getModifiers() & 0xFFFFFFEF);
+		} catch (NoSuchFieldException e) {
+			if (e.getCause().getMessage().equals("modifiers")) {
+				System.err.println("Failed to remove final modifier from " + field);
+			} else {
+				throw e;
+			}
+		}
+		return field;
 	}
 
 	/**
